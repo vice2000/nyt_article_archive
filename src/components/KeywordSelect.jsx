e2c363405484a10e985f05a11eb09b742b36c0f2
Tobias Weiss <contact@tobias-weiss.info>
2019-03-13 22:45:44 +0100

    Filter onSubmit

--- src/components/KeywordSelect.jsx
+++ src/components/KeywordSelect.jsx
@@ -4,20 +4,48 @@ import PropTypes from 'prop-types';
 
 class KeywordSelect extends React.Component {
 
+    state = {
+        initialValue: 'Select Keyword',
+        value: ''
+    }
+
     onChange = (e) => {
-        this.props.filterTeasers(e.target.value);
+        const { value } = e.target;
+        this.props.filterTeasers(value);
+        this.setState({ value });
+    }
+
+    handleSubmit = (e) => {
+        const { clearFilter } = this.props;
+        e.preventDefault();
+        this.setState({ value: '' });
+        clearFilter();
     }
 
     render() {
-        const {keywords, clearFilter} = this.props;
+        const { keywords } = this.props;
+        const inactiveClass = this.state.value ? '' : 'header__input--inactive';
         return (
-            <div className="header__keywordfilter">
-                <select onChange={this.onChange}>
-                    <option disabled>Keywords for Selected Month</option>
-                    {keywords.map(keyword => <option key={keyword} name={keyword}>{keyword}</option>)} 
-                </select>
-                <button onClick={clearFilter}>Clear Filter</button>
-            </div>
+            <form className="header__keywordfilter" onSubmit={this.handleSubmit}>
+                <fieldset className="header__fieldset">
+                    <label className="header__label" htmlFor="keywordselect">Filter by single Keyword</label>
+                    <select
+                        id="keywordselect"
+                        className={'header__input header__input--stretched ' + inactiveClass}
+                        onChange={this.onChange}
+                        value={this.state.value || this.state.initialValue}
+                    >
+                        <option
+                            disabled
+                            value="Select Keyword"
+                        >
+                        Select Keyword
+                        </option>
+                        {keywords.map(keyword => <option key={keyword} name={keyword} value={keyword}>{keyword}</option>)} 
+                    </select>
+                </fieldset>
+                <button className="header__button" type="submit">Clear Filter</button>
+            </form>
         );
     }
 }
