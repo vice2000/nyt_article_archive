885f15eaecb4f7909b64b78cc83c8bfddfc5105c
Tobias Weiß <contact@tobias-weiss.info>
2020-10-13 20:15:28 +0200

    Fix form button position

--- src/components/Datepicker.jsx
+++ src/components/Datepicker.jsx
@@ -3,44 +3,44 @@ import PropTypes from 'prop-types';
 import DateSelector from './DateSelector';
 
 class Datepicker extends React.PureComponent {
+  state = {};
 
-    state = {};
+  setRequestDate = (date) => {
+      this.setState({ date });
+  };
 
-    setRequestDate = (date) => {
-        this.setState( { date } );
-    }
+  handleSubmit = (e) => {
+      e.preventDefault();
+      if (this.state.date) {
+          this.props.getData(this.state.date);
+      }
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        if (this.state.date) {
-            this.props.getData( this.state.date );
-        }
-    }
-
-    render () {
-        const expandedClass = this.props.expanded ? '' : 'header__datepicker--hidden';
-        return (
-            <form
-                action="/"
-                className={'header__datepicker ' + expandedClass}
-                method="POST"
-                onSubmit={this.handleSubmit}
-            >
-                <DateSelector
-                    date={this.setRequestDate}
-                />
-                <button
-                    type="submit"
-                    className="header__button"
-                >Search!</button>
-            </form>
-        );
-    }
+  render() {
+      const expandedClass = this.props.expanded
+          ? ''
+          : 'header__datepicker--hidden';
+      return (
+          <form
+              action="/"
+              className={'header__datepicker ' + expandedClass}
+              method="POST"
+              onSubmit={this.handleSubmit}
+          >
+              <div className="field__wrapper">
+                  <DateSelector date={this.setRequestDate} />
+                  <button type="submit" className="header__button">
+            Search!
+                  </button>
+              </div>
+          </form>
+      );
+  }
 }
 
 Datepicker.propTypes = {
     getData: PropTypes.func,
-    expanded: PropTypes.bool
+    expanded: PropTypes.bool,
 };
 
 export default Datepicker;
--- src/components/KeywordSelect.jsx
+++ src/components/KeywordSelect.jsx
@@ -2,66 +2,69 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class KeywordSelect extends React.PureComponent {
+  state = {
+      initialValue: 'Select Keyword',
+      value: '',
+  };
 
-    state = {
-        initialValue: 'Select Keyword',
-        value: ''
-    }
+  onChange = (e) => {
+      // replace count passed as string literal
+      const value = e.target.value.replace(/(\s\[\d+\])$/g, '');
+      this.props.filterTeasers(value);
+      this.setState({ value });
+  };
 
-    onChange = (e) => {
-        // replace count passed as string literal
-        const value = e.target.value.replace(/(\s\[\d+\])$/g,'');
-        this.props.filterTeasers(value);
-        this.setState({ value });
-    }
+  handleSubmit = (e) => {
+      const { clearFilter } = this.props;
+      e.preventDefault();
+      this.setState({ value: '' });
+      clearFilter();
+  };
 
-    handleSubmit = (e) => {
-        const { clearFilter } = this.props;
-        e.preventDefault();
-        this.setState({ value: '' });
-        clearFilter();
-    }
-
-    render() {
-        const { expanded, keywords, filterKeyword } = this.props;
-        const { value, initialValue } = this.state;
-        const modifierClass = value || filterKeyword ? '' : 'header__input--inactive';
-        const expandedClass = expanded ? '' : 'header__keywordfilter--hidden';
-        return (
-            <form
-                className={'header__keywordfilter ' + expandedClass}
-                onSubmit={this.handleSubmit}
-            >
-                <fieldset className="header__fieldset">
-                    <label className="header__label" htmlFor="keywordselect">Filter by single Keyword</label>
-                    <select
-                        id="keywordselect"
-                        className={'header__input header__input--stretched ' + modifierClass}
-                        onChange={this.onChange}
-                        value={filterKeyword || value || initialValue}
-                    >
-                        <option
-                            disabled
-                            value={filterKeyword || initialValue}
-                        >
-                            {filterKeyword || initialValue}
-                        </option>
-                        {
-                            keywords.map(keyword => {
-                                const { kw, count } = keyword;
-                                return (
-                                    <option key={kw} name={kw} value={kw}>
-                                        {`${kw} [${count}]`}
-                                    </option>
-                                );
-                            })
-                        }
-                    </select>
-                </fieldset>
-                <button className="header__button" type="submit">Clear Filter</button>
-            </form>
-        );
-    }
+  render() {
+      const { expanded, keywords, filterKeyword } = this.props;
+      const { value, initialValue } = this.state;
+      const modifierClass =
+      value || filterKeyword ? '' : 'header__input--inactive';
+      const expandedClass = expanded ? '' : 'header__keywordfilter--hidden';
+      return (
+          <form
+              className={'header__keywordfilter ' + expandedClass}
+              onSubmit={this.handleSubmit}
+          >
+              <div className="field__wrapper">
+                  <fieldset className="header__fieldset">
+                      <label className="header__label" htmlFor="keywordselect">
+              Filter by single Keyword
+                      </label>
+                      <select
+                          id="keywordselect"
+                          className={
+                              'header__input header__input--stretched ' + modifierClass
+                          }
+                          onChange={this.onChange}
+                          value={filterKeyword || value || initialValue}
+                      >
+                          <option disabled value={filterKeyword || initialValue}>
+                              {filterKeyword || initialValue}
+                          </option>
+                          {keywords.map((keyword) => {
+                              const { kw, count } = keyword;
+                              return (
+                                  <option key={kw} name={kw} value={kw}>
+                                      {`${kw} [${count}]`}
+                                  </option>
+                              );
+                          })}
+                      </select>
+                  </fieldset>
+                  <button className="header__button" type="submit">
+            Clear Filter
+                  </button>
+              </div>
+          </form>
+      );
+  }
 }
 
 KeywordSelect.propTypes = {
@@ -69,7 +72,7 @@ KeywordSelect.propTypes = {
     filterTeasers: PropTypes.func,
     clearFilter: PropTypes.func,
     expanded: PropTypes.bool,
-    filterKeyword: PropTypes.string
+    filterKeyword: PropTypes.string,
 };
 
 export default KeywordSelect;
